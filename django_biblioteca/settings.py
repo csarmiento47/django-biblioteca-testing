faa0b2117c87142f7dfd1a477342b869db5db715
Christian Sarmiento <christian.sarmiento@gmail.com>
2026-02-25 23:54:32 -0300

    allowed host modified

--- django_biblioteca/settings.py
+++ django_biblioteca/settings.py
@@ -31,7 +31,7 @@ DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 ALLOWED_HOSTS = [
     'localhost',
     '127.0.0.1',
-    'https://django-biblioteca-testing.onrender.com/',
+    'django-biblioteca-testing.onrender.com/',
 ]
 
 
